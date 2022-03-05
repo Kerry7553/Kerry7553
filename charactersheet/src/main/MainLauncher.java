66ed70ea05af8fb7e35fcfb53348d5a04fa4ce28
Kerry7553 <56826913+Kerry7553@users.noreply.github.com>
2022-03-05 11:32:33 +0000

    Delete charactersheet directory

--- charactersheet/src/main/MainLauncher.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-public class MainLauncher
-{
-    public static void main(final String[] args) {
-        final FormController mainController = new FormController();
-        mainController.display();
-    }
-}
--- charactersheet/src/module-info.java
+++
@@ -1,3 +0,0 @@
-module charactersheet {
-	requires java.desktop;
-}
